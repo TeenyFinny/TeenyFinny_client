74b8758a5cd20394ed70b587c80baa311ec37837
JBLee <rabid28@naver.com>
2025-11-05 12:49:19 +0900

    feat: 푸시 알림 컴포넌트 구현

--- src/app/lib-test/page.tsx
+++ src/app/lib-test/page.tsx
@@ -1,17 +1,19 @@
 import ButtonTest from "@/components/test/ui/button/ButtonTest";
 import CardForTest from "@/components/test/ui/card/CardForTest";
 import InputTest from "@/components/test/ui/input/InputTest";
+import NoticeTest from "@/components/test/ui/notice/NoticeTest";
 
 export default function DesignSystemDemo() {
   return (
     // 화면 전체
-    <div className="w-screen h-dvh bg-neutral-6 overflow-x-hidden">
+    <div className="w-screen h-dvh bg-green-500 overflow-x-hidden">
       {/* 가운데 정렬 + 좌우 24px 패딩 = 375-48=327 */}
       <section className="w-full h-full flex items-center justify-center px-6">
         {/* 버튼 고정폭 래퍼: ButtonTest 내부가 w-[327px] 여도 정확히 맞음 */}
-        <div className="w-[375px]">
-          *<InputTest/>
+        <div className="w-[375px] h-full bg-blue-500">
+          {/* *<InputTest/> */}
           {/* *<ButtonTest /> */}
+          <NoticeTest />
         </div>
       </section>
     </div>
