7a465c276764c0c60aaf3b113f727741c6acc0d2
yangyanghyunjung <hyunjungyang0922@gmail.com>
2025-11-07 10:06:27 +0900

    fix: 버튼명 변경

--- src/components/ui/modal/DeleteConfirmDialog.tsx
+++ src/components/ui/modal/DeleteConfirmDialog.tsx
@@ -18,20 +18,20 @@ interface DeleteConfirmDialogProps {
   trigger?: React.ReactNode
   title?: string
   description?: string
-  cancelText?: string
-  deleteText?: string
-  onCancel?: () => void
-  onDelete?: () => void
+  ltBtnTxt?: string
+  rtBtnTxt?: string
+  onClickLtBtn?: () => void
+  onClickRtBtn?: () => void
 }
 
 export function DeleteConfirmDialog({
   trigger,
   title ,
   description,
-  cancelText,
-  deleteText,
-  onCancel,
-  onDelete,
+  ltBtnTxt,
+  rtBtnTxt,
+  onClickLtBtn,
+  onClickRtBtn,
 }: DeleteConfirmDialogProps) {
   return (
     <AlertDialog>
@@ -39,25 +39,25 @@ export function DeleteConfirmDialog({
       <AlertDialogContent className="w-[270px] p-0 gap-0 rounded-[14px] backdrop-blur-[27px]">
         <AlertDialogHeader className="px-4 pt-6 pb-5 space-y-3 text-center">
           <AlertDialogTitle className="text-center">
-            <span className="text-head-06 text-(--color-neutral-1) ">{title}</span>
+            <span className="text-head-06 text-neutral-1">{title}</span>
             </AlertDialogTitle>
           <AlertDialogDescription className="text-center">
-            <span className="text-body-07 text-(--color-neutral-3)">{description}</span>
+            <span className="text-body-07 text-neutral-3">{description}</span>
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter className="flex-row p-0 m-0 gap-0 ">
           <AlertDialogCancel
-            onClick={onCancel}
+            onClick={onClickLtBtn}
             className="flex-1 m-0 h-12 rounded-none rounded-bl-[14px] border-0  hover:bg-transparent transition-colors"
           >
-            <span className="text-body-04 text-(--color-info)">{cancelText}</span>
+            <span className="text-body-04 text-info">{ltBtnTxt}</span>
           </AlertDialogCancel>
-          <div className="w-px bg-(--color-neutral-5) my-3" />
+          <div className="w-px bg-neutral-5 my-3" />
           <AlertDialogAction
-            onClick={onDelete}
+            onClick={onClickRtBtn}
             className="flex-1 m-0 h-12 rounded-none rounded-br-[14px] border-0 bg-transparent  hover:bg-transparent transition-colors"
           >
-            <span className="text-body-04 text-(--color-error)">{deleteText}</span>
+            <span className="text-body-04 text-error">{rtBtnTxt}</span>
           </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
