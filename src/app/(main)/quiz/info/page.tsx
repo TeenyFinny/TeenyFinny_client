a1a332ae06913237cc15d00394ef274032b73ff7
CHICHIT <beflylee@naver.com>
2025-11-10 16:25:14 +0900

    feat: 퀴즈 정보 페이지 수정

--- src/app/(main)/quiz/info/page.tsx
+++ src/app/(main)/quiz/info/page.tsx
@@ -1,15 +1,15 @@
 "use client"
 
-import Image from "next/image"
-import React, { useEffect, useState } from "react"
+
+import React, { useEffect } from "react"
 import { StateBadge } from "@/components/ui/badge/StateBadge"
 import { BigButtonActivated } from "@/components/ui/button/BigButtonActivated"
 import { BigButtonDisabled } from "@/components/ui/button/BigButtonDisabled"
 import { useRouter } from "next/navigation"
-import { HttpError } from "@/types/axios/httpError.t"
 import api from "@/lib/axios/axios"
 import requests from "@/lib/axios/requests"
 import { useQuizStore } from "@/store/quizStore"
+import { HttpError } from "@/types/axios/httpError.t"
 
 /**
  * QuizStartPage
@@ -19,86 +19,98 @@ import { useQuizStore } from "@/store/quizStore"
  */
 
 export default function Page() {
-  const router = useRouter()
-  const setQuizData = useQuizStore((state) => state.setQuizData)
-  const quizStore = useQuizStore() // 현재 상태 가져오기 (옵션)
-  
-  // ✅ useEffect로 API 요청
-  useEffect(() => {
-    (async () => {
-      try {
-        //const res = await api.get(`${requests.fetchQuiz}?quiz_id=${quizStore.quiz_date}`)
-        const res = await api.get(`${requests.fetchQuiz}?quiz_id=2`)
-        const data = res.data
-        setQuizData(data) // 전역 상태 저장
-      } catch (e) {
-        console.error(e)
-      }
-    })()
-  }, [setQuizData])
+    const router = useRouter()
+    const {
+        setQuizData,
+        quiz_date,
+        streak_days,
+        course_completed,
+        monthly_reward,
+        today_solved,
+        title,
+        info,
+    } = useQuizStore()
 
-  //TODO: 교육과정일 경우와 랜덤일 경우에 따라 퀴즈 ID를 정하는 알고리즘 추가
+    // ✅ useEffect로 API 요청
+    useEffect(() => {
+        (async () => {
+            try {
+                const res = await api.get(`${requests.fetchQuiz}?quiz_id=${quiz_date}`)
+                const data = res.data
+                setQuizData(data) // 전역 상태 저장
+            } catch (e) {
+                const err = e as HttpError
 
-  const quizActive =
-    !quizStore.course_completed &&
-    !quizStore.monthly_reward &&
-    quizStore.today_solved < 2
-  // ---------------------------
-  // 배지 텍스트
-  // ---------------------------
-  const leftBadgeText = quizStore.monthly_reward
-    ? "이번 달 도전 완료"
-    : `${quizStore.streak_days}일 연속 도전!`
+                if (err.statusCode === 403) {
+                    alert(err.message)
+                    router.push("/")
+                } else {
+                    console.error(err)
+                }
+                console.error(e)
+            }
+        })()
+    }, [setQuizData])
 
-  const rightBadgeText = `${quizStore.today_solved + 1} / 2 문제`
+    //TODO: 교육과정일 경우와 랜덤일 경우에 따라 퀴즈 ID를 정하는 알고리즘 추가
 
-  return (
-    <main
-      aria-label="퀴즈 시작 페이지"
-      className="relative w-full max-w-[375px] mx-auto h-full max-h-[800px] bg-[var(--color-primary-4)] font-[var(--font-sans)] flex flex-col items-center overflow-hidden"
-    >
-      {/* ===============================
+    const quizActive = !course_completed && !monthly_reward && today_solved < 2
+    // ---------------------------
+    // 배지 텍스트
+    // ---------------------------
+    const leftBadgeText = monthly_reward
+        ? "이번 달 도전 완료"
+        : `${streak_days}일 연속 도전!`
+
+    const rightBadgeText = `${today_solved + 1} / 2 문제`
+
+    return (
+        <main
+            aria-label="퀴즈 시작 페이지"
+            className="relative w-full max-w-[375px] mx-auto h-full max-h-[800px] bg-[var(--color-primary-4)] font-[var(--font-sans)] flex flex-col items-center overflow-hidden"
+        >
+            {/* ===============================
           카드 영역 (중앙 콘텐츠)
          =============================== */}
-      <div className="relative w-[327px] h-[490px] mx-auto bg-[var(--color-neutral-7)] rounded-[16px] shadow-[0_16px_64px_-32px_rgba(0,0,0,0.16)] flex flex-col items-center pt-[160px] pb-[80px] mb-[32px]">
-        {/* 좌측 배지 */}
-        <div className="absolute top-[20px] left-[16px]">
-          <StateBadge enabled={true} label={leftBadgeText} onClick={() => {}} />
-        </div>
+            <div className="relative w-[327px] h-[490px] mx-auto bg-[var(--color-neutral-7)] rounded-[16px] shadow-[0_16px_64px_-32px_rgba(0,0,0,0.16)] flex flex-col items-center pt-[160px] pb-[80px] mb-[32px]">
+                {/* 좌측 배지 */}
+                <div className="absolute top-[20px] left-[16px]">
+                    <StateBadge enabled={true} label={leftBadgeText} onClick={() => { }} />
+                </div>
 
-        {/* 우측 배지 */}
-        <div className="absolute top-[20px] right-[16px]">
-          <StateBadge enabled={false} label={rightBadgeText} onClick={() => {}} />
-        </div>
+                {/* 우측 배지 */}
+                <div className="absolute top-[20px] right-[16px]">
+                    <StateBadge enabled={false} label={rightBadgeText} onClick={() => { }} />
+                </div>
 
-       {/* 상단 제못 텍스트 */}
-  {quizActive && (
-    <p className="absolute top-[67px] left-[33px] text-center text-head-00 font-bold text-[var(--color-neutral-1)] w-[260px] mb-8 leading-relaxed">
-      {quizStore.title}
-    </p>
-)}
+                {/* 상단 제목 텍스트 */}
+                {quizActive && (
+                    <p className="absolute top-[67px] left-[33px] text-center text-head-00 font-bold text-[var(--color-neutral-1)] w-[260px] mb-8 leading-relaxed">
+                        {title}
+                    </p>
+                )}
 
 
-        {/* 중단 설명 텍스트 */}
-        {quizActive && (
-           <p className="absolute top-[154px] left-[36px] text-left text-body-06 font-regular text-[var(--color-neutral-1)] w-[254px] mb-8 leading-relaxed">
-            {quizStore.info}
-            </p>
-       ) }
+                {/* 중단 설명 텍스트 */}
+                {quizActive && (
+                    <p className="absolute top-[154px] left-[36px] text-left text-body-06 font-bold text-[var(--color-neutral-1)] w-[254px] mb-8 leading-loose break-keep tracking-wide">
+                        {info}
+                    </p>
+                )}
 
-      </div>
+            </div>
 
-      {/* 시작 버튼 */}
-      <div className="w-[327px]">
-        {quizActive ? (
-          <BigButtonActivated
-            label="문제 풀기"
-            onClick={() => router.push("/quiz/question")}
-          />
-        ) : (
-          <BigButtonDisabled label={"오늘의 퀴즈를 모두 풀었어요!"} onClick={() => {}} />
-        )}
-      </div>
-    </main>
-  )
+            {/* 시작 버튼 */}
+            <div className="w-[327px]">
+                {quizActive ? (
+                    <BigButtonActivated
+                        label="문제 풀기"
+                        onClick={() => router.push("/quiz/question")}
+                    />
+                ) : (
+                    <BigButtonDisabled label={"오늘의 퀴즈를 모두 풀었어요!"} onClick={() => { }} />
+                )}
+            </div>
+        </main>
+    )
 }
--- src/app/(main)/quiz/page.tsx
+++ src/app/(main)/quiz/page.tsx
@@ -23,13 +23,13 @@ export default function Page() {
   const setQuizData = useQuizStore((state) => state.setQuizData)
   const quizStore = useQuizStore() // 현재 상태 가져오기 (옵션)
 
-//  // ✅ state 선언부
-//   const [streakDays, setStreakDays] = useState(0)
-//   const [courseCompleted, setCourseCompleted] = useState(false)
-//   const [quizDate, setQuizDate] = useState(0)
-//   const [monthlyReward, setMonthlyReward] = useState(false)
-//   const [todaySolved, setTodaySolved] = useState(0)
-//   const [quizActive, setQuizActive] = useState(true)
+  //  // ✅ state 선언부
+  //   const [streakDays, setStreakDays] = useState(0)
+  //   const [courseCompleted, setCourseCompleted] = useState(false)
+  //   const [quizDate, setQuizDate] = useState(0)
+  //   const [monthlyReward, setMonthlyReward] = useState(false)
+  //   const [todaySolved, setTodaySolved] = useState(0)
+  //   const [quizActive, setQuizActive] = useState(true)
 
   // ✅ useEffect로 API 요청
   useEffect(() => {
@@ -39,6 +39,14 @@ export default function Page() {
         const data = res.data
         setQuizData(data) // 전역 상태 저장
       } catch (e) {
+        const err = e as HttpError
+
+        if (err.statusCode === 403) {
+          alert(err.message)
+          router.push("/")
+        } else {
+          console.error(err)
+        }
         console.error(e)
       }
     })()
@@ -68,24 +76,24 @@ export default function Page() {
       <div className="relative w-[327px] h-[490px] mx-auto bg-[var(--color-neutral-7)] rounded-[16px] shadow-[0_16px_64px_-32px_rgba(0,0,0,0.16)] flex flex-col items-center pt-[160px] pb-[80px] mb-[32px]">
         {/* 좌측 배지 */}
         <div className="absolute top-[20px] left-[16px]">
-          <StateBadge enabled={true} label={leftBadgeText} onClick={() => {}} />
+          <StateBadge enabled={true} label={leftBadgeText} onClick={() => { }} />
         </div>
 
         {/* 우측 배지 */}
         <div className="absolute top-[20px] right-[16px]">
-          <StateBadge enabled={false} label={rightBadgeText} onClick={() => {}} />
+          <StateBadge enabled={false} label={rightBadgeText} onClick={() => { }} />
         </div>
 
-       {/* 상단 설명 텍스트 (course_completed가 false일 때만 표시) */}
-  {!quizStore.course_completed && (
-    <p className="absolute top-[85px] left-[48px] text-center text-head-04 font-bold text-[var(--color-neutral-1)] w-[231px] mb-8 leading-relaxed">
-      15일간의 퀴즈 교육 과정을 전부 마치면<br />
-      <span className="text-[var(--color-primary-1)] font-bold">
-        주식 크레딧
-      </span>
-      을 얻을 수 있어요!
-    </p>
-)}
+        {/* 상단 설명 텍스트 (course_completed가 false일 때만 표시) */}
+        {!quizStore.course_completed && (
+          <p className="absolute top-[85px] left-[48px] text-center text-head-04 font-bold text-[var(--color-neutral-1)] w-[231px] mb-8 leading-relaxed">
+            15일간의 퀴즈 교육 과정을 전부 마치면<br />
+            <span className="text-[var(--color-primary-1)] font-bold">
+              주식 크레딧
+            </span>
+            을 얻을 수 있어요!
+          </p>
+        )}
 
         {/* 중앙 이미지 */}
         <div className="absolute top-[135px] w-[262px] h-[262px] mb-8">
@@ -99,25 +107,25 @@ export default function Page() {
         </div>
 
         {/* 하단 설명 텍스트 */}
-<p className="absolute top-[407px] left-[45px] text-center text-head-04 font-bold text-[var(--color-neutral-1)] w-[237px] mb-8 leading-relaxed">
-  {quizStore.monthly_reward ? (
-    <>
-      이번 달의{" "}
-      <span className="text-[var(--color-primary-1)] font-bold">
-        용돈조르기권
-      </span>
-      을 <br />이미 받아갔어요!
-    </>
-  ) : (
-    <>
-      3일 연속으로 퀴즈를 풀면 한 달에 한 번, <br />
-      <span className="text-[var(--color-primary-1)] font-bold">
-        용돈조르기권
-      </span>
-      을 얻을 수 있어요!
-    </>
-  )}
-</p>
+        <p className="absolute top-[407px] left-[45px] text-center text-head-04 font-bold text-[var(--color-neutral-1)] w-[237px] mb-8 leading-relaxed">
+          {quizStore.monthly_reward ? (
+            <>
+              이번 달의{" "}
+              <span className="text-[var(--color-primary-1)] font-bold">
+                용돈조르기권
+              </span>
+              을 <br />이미 받아갔어요!
+            </>
+          ) : (
+            <>
+              3일 연속으로 퀴즈를 풀면 한 달에 한 번, <br />
+              <span className="text-[var(--color-primary-1)] font-bold">
+                용돈조르기권
+              </span>
+              을 얻을 수 있어요!
+            </>
+          )}
+        </p>
       </div>
 
       {/* 시작 버튼 */}
@@ -128,7 +136,7 @@ export default function Page() {
             onClick={() => router.push("/quiz/info")}
           />
         ) : (
-          <BigButtonDisabled label={"오늘의 퀴즈를 모두 풀었어요!"} onClick={() => {}} />
+          <BigButtonDisabled label={"오늘의 퀴즈를 모두 풀었어요!"} onClick={() => { }} />
         )}
       </div>
     </main>
