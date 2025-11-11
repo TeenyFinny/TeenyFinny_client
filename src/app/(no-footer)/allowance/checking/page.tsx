facddc764d067d88a6097b0bd71a6d52dadb35fb
mingQ28 <eo77si9@naver.com>
2025-11-11 17:30:58 +0900

    feat: 상품 약관 페이지 구현

--- src/app/(no-footer)/allowance/checking/page.tsx
+++ src/app/(no-footer)/allowance/checking/page.tsx
@@ -4,7 +4,7 @@ import Step01Intro from "./Step01Intro";
 import Step02Checklist from "./Step02Checklist";
 import Step03AuthAgreement from "./Step03AuthAgreement";
 import Step04Auth from "./Step04Auth";
-import Step05Loading from "./Step05Loading";
+import Step06ProductAgreement from "./Step06ProductAgreement";
 export default function Page() {
   const [step, setStep] = useState(1);
   return (
@@ -12,9 +12,9 @@ export default function Page() {
       {step === 1 && <Step01Intro onNext={() => setStep(2)} />}
       {step === 2 && <Step02Checklist onNext={() => setStep(3)} />}
       {step == 3 &&  <Step03AuthAgreement onNext={() => setStep(4)} />}
-      {step == 4 &&  <Step04Auth onNext={() => setStep(5)} />}
-      {step === 5 && (
-        <Step05Loading
+      {step == 4 &&  <Step04Auth onNext={() => setStep(6)} />}
+      {step === 6 && (
+        <Step06ProductAgreement
           onNext={() => alert("아직 다음 단계는 구현되지 않았어요!")}
         />
       )}
