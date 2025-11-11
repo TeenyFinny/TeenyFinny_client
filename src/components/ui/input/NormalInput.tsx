f64110569909f2bdaafdb2a5232ac4b28acf1edc
mingQ28 <eo77si9@naver.com>
2025-11-11 09:07:56 +0900

    design: 피그마에 나온 값으로 텍스트 폰트 수정

--- src/components/ui/input/NormalInput.tsx
+++ src/components/ui/input/NormalInput.tsx
@@ -73,7 +73,7 @@ export function NormalInput({ label, placeholder, value, onChange }: TextInputPr
         placeholder={placeholder}
         className="left-0 right-0 top-0 bottom-0 w-[320px] h-[64px] rounded-[6px] 
             bg-neutral-7 shadow-[0_8px_24px_-6px_rgba(0,0,0,0.04)] px-[16px] 
-            text-body-01 leading-[19px] tracking-[-0.6px] text-neutral-1 
+            text-body-04 leading-[19px] tracking-[-0.6px] text-neutral-1 
             placeholder:text-neutral-3 focus:outline-none focus-within:ring-2 focus-within:ring-primary-1"
       />
     </div>
