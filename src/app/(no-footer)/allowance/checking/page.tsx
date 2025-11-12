3d289b5cc8ec4b8e092defe01a35c6c68de445a7
mingQ28 <eo77si9@naver.com>
2025-11-12 20:59:04 +0900

    feat: 계좌 개설 완료 페이지 구현 및 페이지 전환 로직 추가

--- src/app/(no-footer)/allowance/checking/page.tsx
+++ src/app/(no-footer)/allowance/checking/page.tsx
@@ -6,6 +6,7 @@ import Step03AuthAgreement from "./Step03AuthAgreement";
 import Step04Auth from "./Step04Auth";
 import Step06ProductAgreement from "./Step06ProductAgreement";
 import Step07ChildInfoInput from "./Step07ChildInfo";
+import Steop08Complete from "./Step08Complete";
 export default function Page() {
   const [step, setStep] = useState(1);
   return (
@@ -15,11 +16,8 @@ export default function Page() {
       {step == 3 &&  <Step03AuthAgreement onNext={() => setStep(4)} />}
       {step == 4 &&  <Step04Auth onNext={() => setStep(6)} />}
       {step == 6 &&  <Step06ProductAgreement onNext={() => setStep(7)} />}
-      {step === 7 && (
-        <Step07ChildInfoInput
-          onNext={() => alert("아직 다음 단계는 구현되지 않았어요!")}
-        />
-      )}
+      {step == 7 &&  <Step07ChildInfoInput onNext={() => setStep(8)} />}
+      {step === 8 &&  <Steop08Complete/> }
     </>
   );
 }
