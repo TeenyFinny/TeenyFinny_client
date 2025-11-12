f0defddef25bd4d00f76f851aa7a8e82b1b874b1
hyojeongbae <bhj09270@naver.com>
2025-11-12 12:28:13 +0900

    fix: 잘못된 props 삭제

--- src/app/(no-footer)/goal/create/page.tsx
+++ src/app/(no-footer)/goal/create/page.tsx
@@ -90,10 +90,8 @@ export default function GoalSettingPage() {
             value={savingDay}
             placeholder="1"
             onChange={setSavingDay}
+            isNumeric
             unit="일"
-            min={1}
-            max={31}
-            step={1}
           />
         </div>
 
