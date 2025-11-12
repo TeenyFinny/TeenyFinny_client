924c7ea98f1558c955d5b82f2bbe622374d1596f
mingQ28 <eo77si9@naver.com>
2025-11-12 16:04:00 +0900

    feat: 자녀 정보 입력 페이지 전환 로직 추가

--- src/app/(no-footer)/allowance/checking/page.tsx
+++ src/app/(no-footer)/allowance/checking/page.tsx
@@ -5,6 +5,7 @@ import Step02Checklist from "./Step02Checklist";
 import Step03AuthAgreement from "./Step03AuthAgreement";
 import Step04Auth from "./Step04Auth";
 import Step06ProductAgreement from "./Step06ProductAgreement";
+import Step07ChildInfoInput from "./Step07ChildInfo";
 export default function Page() {
   const [step, setStep] = useState(1);
   return (
@@ -13,8 +14,9 @@ export default function Page() {
       {step === 2 && <Step02Checklist onNext={() => setStep(3)} />}
       {step == 3 &&  <Step03AuthAgreement onNext={() => setStep(4)} />}
       {step == 4 &&  <Step04Auth onNext={() => setStep(6)} />}
-      {step === 6 && (
-        <Step06ProductAgreement
+      {step == 6 &&  <Step06ProductAgreement onNext={() => setStep(7)} />}
+      {step === 7 && (
+        <Step07ChildInfoInput
           onNext={() => alert("아직 다음 단계는 구현되지 않았어요!")}
         />
       )}
