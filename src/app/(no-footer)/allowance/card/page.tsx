75a6a9093c0d1d65f2ec73bf40fd2a5179c2542e
mingQ28 <eo77si9@naver.com>
2025-11-13 21:45:56 +0900

    feat: step05 페이지 전환 로직 추가

--- src/app/(no-footer)/allowance/card/page.tsx
+++ src/app/(no-footer)/allowance/card/page.tsx
@@ -4,6 +4,7 @@ import Step01CardIntro from "./Step01CardIntro";
 import Step02CardAuthAgreement from "./Step02CardAuthAgreement";
 import Step03CardAuth from "./Step03CardAuth";
 import Step04CardOptions from "./Step04CardOptions";
+import Steop05CardComplete from "./Step05CardComplete";
 export default function Page() {
   const [step, setStep] = useState(1);
   return (
@@ -11,11 +12,8 @@ export default function Page() {
       {step === 1 && <Step01CardIntro onNext={() => setStep(2)} />}
       {step === 2 && <Step02CardAuthAgreement onNext={() => setStep(3)} />}
       {step === 3 && <Step03CardAuth onNext={() => setStep(4)} />}
-      {step === 4 && (
-        <Step04CardOptions
-          onNext={() => alert("아직 다음 단계는 구현되지 않았어요!")}
-        />
-      )}
+      {step === 4 && <Step04CardOptions onNext={() => setStep(5)} />}
+      {step === 5 && <Steop05CardComplete/> }
     </>
   );
 }
