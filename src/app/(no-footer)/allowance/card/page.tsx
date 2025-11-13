18583e1621b5d289875d039e063829603b9d7d6e
mingQ28 <eo77si9@naver.com>
2025-11-13 12:58:56 +0900

    feat: step02 페이지 전환 로직 추가

--- src/app/(no-footer)/allowance/card/page.tsx
+++ src/app/(no-footer)/allowance/card/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 import { useState } from "react";
 import Step01CardIntro from "./Step01CardIntro";
+import Step02CardAuthAgreement from "./Step02CardAuthAgreement";
 export default function Page() {
   const [step, setStep] = useState(1);
   return (
     <>
       {step === 1 && <Step01CardIntro onNext={() => setStep(2)} />}
-      {/* {step === 2 && (
-        <Step02
+      {step === 2 && (
+        <Step02CardAuthAgreement
           onNext={() => alert("아직 다음 단계는 구현되지 않았어요!")}
         />
-      )} */}
+      )}
     </>
   );
 }
