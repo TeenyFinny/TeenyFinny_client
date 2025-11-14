4ca60a5472f6b00bfa9f35ed40500afb6123cf1e
mingQ28 <eo77si9@naver.com>
2025-11-14 09:28:24 +0900

    chore: 컴포넌트명 오타 수정

--- src/app/(no-footer)/allowance/card/page.tsx
+++ src/app/(no-footer)/allowance/card/page.tsx
@@ -4,7 +4,7 @@ import Step01CardIntro from "./Step01CardIntro";
 import Step02CardAuthAgreement from "./Step02CardAuthAgreement";
 import Step03CardAuth from "./Step03CardAuth";
 import Step04CardOptions from "./Step04CardOptions";
-import Steop05CardComplete from "./Step05CardComplete";
+import Step05CardComplete from "./Step05CardComplete";
 export default function Page() {
   const [step, setStep] = useState(1);
   return (
@@ -13,7 +13,7 @@ export default function Page() {
       {step === 2 && <Step02CardAuthAgreement onNext={() => setStep(3)} />}
       {step === 3 && <Step03CardAuth onNext={() => setStep(4)} />}
       {step === 4 && <Step04CardOptions onNext={() => setStep(5)} />}
-      {step === 5 && <Steop05CardComplete/> }
+      {step === 5 && <Step05CardComplete/> }
     </>
   );
 }
