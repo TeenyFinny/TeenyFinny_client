e83e8475473c92f06c949e4b883d7f5c1c7f6414
hyojeongbae <bhj09270@naver.com>
2025-11-14 14:36:01 +0900

    feat: cleanup 함수 추가

--- src/app/(no-footer)/notice/page.tsx
+++ src/app/(no-footer)/notice/page.tsx
@@ -14,17 +14,37 @@ interface Notification {
 export default function NotificationsPage() {
   const [notifications, setNotifications] = useState<Notification[]>([])
 
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const res = await api.get(requests.fetchNotice)
+  //       setNotifications(res.data.notifications)
+  //     } catch (error) {
+  //       console.error("알림 데이터를 불러오지 못했습니다:", error)
+  //     }
+  //   }
+  //   fetchData()
+  // }, [])
+
   useEffect(() => {
+    const controller = new AbortController();
     const fetchData = async () => {
       try {
-        const res = await api.get(requests.fetchNotice)
-        setNotifications(res.data.notifications)
+        const res = await api.get(requests.fetchNotice, { signal: controller.signal });
+        setNotifications(res.data.notifications);
       } catch (error) {
-        console.error("알림 데이터를 불러오지 못했습니다:", error)
+
+        console.error("알림 데이터를 불러오지 못했습니다:", error);
+
       }
-    }
-    fetchData()
-  }, [])
+    };
+
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
 
   // ✅ 클릭 시 읽음 처리
   const handleRead = (index: number) => {
@@ -48,9 +68,8 @@ export default function NotificationsPage() {
           <div
             key={index}
             onClick={() => handleRead(index)} // ✅ 클릭 시 상태 변경
-            className={`cursor-pointer transition-colors ${
-              n.isRead ? "bg-transparent" : "bg-[rgba(0,103,172,0.15)]"
-            }`}
+            className={`cursor-pointer transition-colors ${n.isRead ? "bg-transparent" : "bg-[rgba(0,103,172,0.15)]"
+              }`}
           >
             <NotificationItem
               message={n.title}
