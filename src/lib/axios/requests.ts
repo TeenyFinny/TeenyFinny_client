03514b045bbbf46c1931c387950b776ca2d70e82
yes2489 <yes24150@gmail.com>
2025-11-17 12:21:29 +0900

    refactor: 엔드포인트 kebab-case로 수정

--- src/lib/axios/requests.ts
+++ src/lib/axios/requests.ts
@@ -5,7 +5,7 @@ const requests = {
   fetchHome: `/home/parent`, // TODO: /home/parent => 자녀 0, /home/noChild => 자녀 0, 자녀 대시보드 생성 후 /home 으로 통합 예정
   login: `/auth/login`,
   signup: `/auth/signup`,
-  verifySimplePassword: `/auth/simplePassword/verify`,
+  verifySimplePassword: `/auth/simple-password/verify`,
   verifyFamilyOtp: `/auth/otp`, // OTP 검증
   fetchGoal: `/goal`,
   fetchProgress: "/quiz/progress",
