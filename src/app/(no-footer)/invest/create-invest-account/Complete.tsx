d846f396426c663f033b2de4bde0ddeac063776b
yangyanghyunjung <hyunjungyang0922@gmail.com>
2025-11-18 09:06:23 +0900

    fix: 버튼 위치 수정

--- src/app/(no-footer)/invest/create-invest-account/Complete.tsx
+++ src/app/(no-footer)/invest/create-invest-account/Complete.tsx
@@ -34,7 +34,7 @@ export default function Complete() {
       </div>
 
       {/* Bottom Button */}
-      <div className="w-full mt-28">
+      <div className="fixed bottom-[56px] w-full max-w-[327px]">
         <BigButtonActivated label="투자 홈으로 돌아가기" onClick={() => router.push("/invest")}></BigButtonActivated>
       </div>
     </div>
