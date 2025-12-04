a916849c22d00d0d04d48308d9b4aaad63bb809b
CHICHIT <beflylee@naver.com>
2025-12-04 16:05:48 +0900

    feat: UI 수정, 이미지 추가

--- src/app/(main)/quiz/info/page.tsx
+++ src/app/(main)/quiz/info/page.tsx
@@ -85,100 +85,101 @@ export default function Page() {
 
 
   // ---------------------------
-  // 배지 텍스트
-  // ---------------------------
-  const leftBadgeText = `${quizDate + 1}일차 도전!`
-
-  const rightBadgeText = `${todaySolved + 1} / 2 문제`
-
-  // **...** 패턴을 찾아 특정 스타일의 <span> 태그로 변환하는 함수
-  const renderWithHighlights = (line) => {
-    // 1. ** 와 ** 사이의 모든 문자열을 찾고, <span> 태그로 감싸줍니다.
-    const htmlString = line.replace(
-      /\*\*(.*?)\*\*/g, // 정규표현식: **(키워드)** 패턴
-      // 키워드에 적용할 강조 스타일: 굵게(font-extrabold) + 눈에 띄는 색상(예: text-yellow-500)
-      '<span class="font-extrabold text-primary-2">$1</span>'
-    );
-    // 2. HTML 문자열을 렌더링하기 위해 dangerouslySetInnerHTML 사용
-    return <div dangerouslySetInnerHTML={{ __html: htmlString }} />;
-  };
-
-  return (
-    <main
-      aria-label="퀴즈 정보 페이지"
-      className="relative w-full max-w-[375px] mx-auto h-full max-h-[800px] bg-primary-4 font-sans flex flex-col items-center overflow-hidden"
-    >
-      {/* ===============================
-          카드 영역 (중앙 콘텐츠)
-         =============================== */}
-      <div className="relative w-[327px] h-[490px] mx-auto bg-neutral-7 rounded-[16px] shadow-[0_16px_64px_-32px_rgba(0,0,0,0.16)] flex flex-col items-center pt-[160px] pb-[80px] mb-[32px]">
-        {/* 좌측 배지 */}
-        <div className="absolute top-[20px] left-[16px]">
-          <StateBadge enabled={true} label={leftBadgeText} onClick={() => { }} />
-        </div>
-
-        {/* 우측 배지 */}
-        <div className="absolute top-[20px] right-[16px]">
-          <StateBadge enabled={false} label={rightBadgeText} onClick={() => { }} />
-        </div>
-
-        {/* 상단 제목 텍스트 */}
-        {/* {(
-          <p className="absolute top-[67px] left-[33px] text-center text-head-00 font-bold text-primary-1 w-[260px] mb-8 leading-relaxed">
-            {title}
-          </p>
-        )} */}
-
-
-
-
-        {/* 중단 설명 텍스트 */}
-        {(
-          <div className="absolute top-[64px] left-[36px] w-[254px] mb-8">
-            {info.split("\n").map((line, index) => {
-              // 빈 문자열이 있다면 렌더링하지 않고 넘어갑니다.
-              if (line.trim() === '') {
-                return null;
-              }
-
-              // 라인이 '•' 기호로 시작하는지 확인합니다.
-              const isBody = line.trim().startsWith('•');
-
-              // 텍스트에서 '•' 기호를 제거합니다.
-              const content = line.trim().replace(/^•\s*/, '');
-              // '^•\s*'는 줄 시작 부분의 '•'와 그 뒤의 공백(있을 경우)을 제거합니다.
-
-              return (
-                <p
-                  key={index}
-                  className={`
-                        text-left leading-snug tracking-wide break-all whitespace-normal
-                        ${!isBody
-                      // ✅ 짝수 Index (제목): 큰 글씨, 두꺼운 볼드, 위 여백 추가
-                      ? 'text-head-01 font-extrabold text-primary-1 mt-6'
-                      // ✅ 홀수 Index (설명): 일반 크기, 일반 볼드, 들여쓰기
-                      : 'text-head-03 font-bold text-neutral-1 indent-2'
-                    }
-                        mb-3 // 각 라인 아래 여백
-                    `}
-                >
-                  {/* 라인의 내용만 렌더링 */}
-                  {renderWithHighlights(content)}
-                </p>
-              );
-            })}
-            {/* 주식 관련 이미지 삽입 위치 */}
-            <div className="absolute top-[265px] w-full flex justify-center">
-              <img
-                src="/images/quiz/image_quiz_1.jpg" // ✅ 이미지 경로 설정 (public 폴더 기준)
-                alt="어린이를 위한 주식 투자 설명 일러스트"
-                className="w-40 h-40 object-contain" // ✅ 이미지 크기 및 스타일 조정
-              />
-            </div>
+    // 배지 텍스트
+    // ---------------------------
+    const leftBadgeText = `${quizDate + 1}일차 도전!`
+  
+    const rightBadgeText = `${todaySolved + 1} / 2 문제`
+  
+    // **...** 패턴을 찾아 특정 스타일의 <span> 태그로 변환하는 함수
+    const renderWithHighlights = (line) => {
+      // 1. ** 와 ** 사이의 모든 문자열을 찾고, <span> 태그로 감싸줍니다.
+      const htmlString = line.replace(
+        /\*\*(.*?)\*\*/g, // 정규표현식: **(키워드)** 패턴
+        // 키워드에 적용할 강조 스타일: 굵게(font-extrabold) + 눈에 띄는 색상(예: text-yellow-500)
+        '<span class="text-head-03 font-bold text-neatural-1">$1</span>'
+      ); 
+      // 2. HTML 문자열을 렌더링하기 위해 dangerouslySetInnerHTML 사용
+      return <div dangerouslySetInnerHTML={{ __html: htmlString }} />;
+    };
+  
+    return (
+      <main
+        aria-label="퀴즈 정보 페이지"
+        className="relative w-full max-w-[375px] mx-auto h-full max-h-[800px] bg-primary-4 font-sans flex flex-col items-center overflow-hidden"
+      >
+        {/* ===============================
+            카드 영역 (중앙 콘텐츠)
+           =============================== */}
+        <div className="relative w-[327px] h-[490px] mx-auto bg-neutral-7 rounded-[16px] shadow-[0_16px_64px_-32px_rgba(0,0,0,0.16)] flex flex-col items-center pt-[160px] pb-[80px] mb-[32px]">
+          {/* 좌측 배지 */}
+          <div className="absolute top-[20px] left-[16px]">
+            <StateBadge enabled={true} label={leftBadgeText} onClick={() => { }} />
           </div>
-        )}
-
-      </div>
+  
+          {/* 우측 배지 */}
+          <div className="absolute top-[20px] right-[16px]">
+            <StateBadge enabled={false} label={rightBadgeText} onClick={() => { }} />
+          </div>
+  
+          {/* 상단 제목 텍스트 */}
+          {/* {(
+            <p className="absolute top-[67px] left-[33px] text-center text-head-00 font-bold text-primary-1 w-[260px] mb-8 leading-relaxed">
+              {title}
+            </p>
+          )} */}
+  
+  
+  
+  
+          {/* 중단 설명 텍스트 */}
+          {(
+            <div className="absolute top-[48px] left-[36px] w-[254px] mb-12">
+              {info.split("\n").map((line, index) => {
+                // 빈 문자열이 있다면 렌더링하지 않고 넘어갑니다.
+                if (line.trim() === '') {
+                  return null;
+                }
+  
+                // 라인이 '•' 기호로 시작하는지 확인합니다.
+                const isBody = line.trim().startsWith('•');
+  
+                // 텍스트에서 '•' 기호를 제거합니다.
+                const content = line.trim().replace(/^•\s*/, '');
+                // '^•\s*'는 줄 시작 부분의 '•'와 그 뒤의 공백(있을 경우)을 제거합니다.
+  
+                return (
+                  <p
+                    key={index}
+                    className={`
+                          text-left leading-snug tracking-wide break-all whitespace-normal
+                          ${!isBody
+                        // ✅ 짝수 Index (제목): 큰 글씨, 두꺼운 볼드, 위 여백 추가
+                        ? 'text-head-01 font-extrabold text-primary-1 mt-6'
+                        // ✅ 홀수 Index (설명): 일반 크기, 일반 볼드, 들여쓰기
+                        : 'text-body-04 font-medium text-neutral-1 indent-0'
+                      }
+                          mb-3 // 각 라인 아래 여백
+                      `}
+                  >
+                    {/* 라인의 내용만 렌더링 */}
+                    {renderWithHighlights(content)}
+                  </p>
+                );
+              })}
+              {/* 주식 관련 이미지 삽입 위치 */}
+              <div className="absolute top-[265px] w-full flex justify-center">
+                <img
+                  src={`/images/quiz/quizinfo_${quizDate * 2 + todaySolved + 1}.png`} // ✅ 이미지 경로 설정 (public 폴더 기준)
+                  
+                  alt="어린이를 위한 일러스트"
+                  className="w-40 h-40 object-contain" // ✅ 이미지 크기 및 스타일 조정
+                />
+              </div>
+            </div>
+          )}
+  
+        </div>
 
       {/* 시작 버튼 */}
       <div className="w-[327px]">
