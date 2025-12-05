0651e062087240a7812c0e0119bc6bd5078a672e
CHICHIT <beflylee@naver.com>
2025-12-05 19:20:14 +0900

    fix: 미사용 임포트 삭제

--- src/components/custom/home/parent-dashboard/ParentWalletCard.tsx
+++ src/components/custom/home/parent-dashboard/ParentWalletCard.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { ChevronRight } from "lucide-react";
 import { MiddleButtonActivated } from "@/components/ui/button/MiddleButtonActivated";
 
 interface ParentWalletCardProps {
